dcdbb27cc43b5d5b3329c58680155f79710e29dc
karama gueye <karamagueye04@gmail.com>
2024-01-02 10:32:21 -0800

    calculatrice

---
+++ calculatrice/calculatrice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+nombre1 = input("Veuillez entrer un premier nombre: ")
+nombre2 = input("Veuillez entrer un deuxieme nombre: ")
+
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+
+resultat = f"Le resultat de l'addition du nombre {nombre1} avec le nombre {nombre2} est egal a {nombre1 + nombre2}"
+print(resultat)
